68a9c5ff780b877b55f8ce09ee6c66c0b9bc623c
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-10-10 11:47:10 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,90 @@
+criaCartao(
+    'matemática ',
+    'qual é o seno 60',
+    'raiz de 3/2',)
+criaCartao(
+    'matemática',
+    'qual é o seno de 30',
+    '1/2',)
+criaCartao(
+    'história',
+    'quem ganhou a primeira guerra',
+    'a tríplice aliança',)
+criaCartao(
+    'História',
+    'quem foi o primeiro presidente do Brasil',
+    'Deodoro Fonseca',)
+criaCartao(
+    'História',
+    'Quem ganha a segunda guerra',
+    'Os aliados',)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da thailandia',
+    'Bangkok',)
+criaCartao(
+    'Geografia',
+    'Qual é a capital dos EUA',
+    'Washington',)
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente',
+    'Ásia',)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'Paris',)
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil',
+    'Brasília',)
+    
+criaCartao(
+    'geopolitica',
+    'qual as maiores 2 potências do mundo atualmente?',
+    'Estados Unidos e China',)
+    
+criaCartao(
+    'conhecimento geral',
+    'onde foi feito as olimpíadas de 2024?',
+    'em Paris na França',)
+    
+criaCartao(
+    'conhecimento geral',
+    'onde será feito as olimpíadas de 2028?',
+    'em Los Angeles nos Estados Unidos',)
+    
+criaCartao(
+    'conhecimento geral',
+    'qual ano foi feito as olimpíadas no Rio de janeiro no Brasil?',
+    '2016',)
+    
+criaCartao(
+    'geopolitica',
+    'quem são os dois principais candidatos a presidência atuais dos Estados Unidos?',
+    'Donald Trump e Kamala Harris',)
+    
+criaCartao(
+    'química',
+    'quantos elétrons o Carbono possui em sua camada de valência?',
+    '4',)
+    
+criaCartao(
+    'química',
+    'O que significa dizer que uma cadeia carbônica é insaturada?',
+    'que ela possui ligações duplas ou triplas',)
+    
+criaCartao(
+    'química',
+    'em uma representação Bond Line qual elemento não é representado junto ao carbono?',
+    'Hidrogênio',)
+    
+criaCartao(
+    'química',
+    'quantos elétrons o Oxigênio possui em sua camada de valência?',
+    '6',)
+    
+criaCartao(
+    'química',
+    'quantas ligações o Hidrogênio precisa fazer para se estabilizar?',
+    '1',)
