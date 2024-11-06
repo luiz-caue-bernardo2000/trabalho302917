4821821ea67e676209faaa8ff74f9c1309f0f00b
luiz-caue-bernardo2000 <161481188+luiz-caue-bernardo2000@users.noreply.github.com>
2024-11-06 08:00:35 -0300

    pagina2.js

--- pagina2.js
+++ pagina2.js
@@ -21,8 +21,7 @@ function mostraResposta (card){
     const resposta = card.querySelector('.resposta-cartao');
     if(resposta.style.display==='none'){
         resposta.style.display='block';
-    }
-        
+    }   
     else{
     resposta.style.display='none';
     }
